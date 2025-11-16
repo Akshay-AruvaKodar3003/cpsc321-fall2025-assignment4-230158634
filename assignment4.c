64d224a898d1dc443bf13333ec9c79272fa33d66
Akshay Arulkrishnan <akshayaknkl@gmail.com>
2025-11-15 20:46:45 -0800

    created required program skeleton

---
+++ assignment4.c
@@ -0,0 +1,21 @@
+/* 
+Akshay Arulkrishnan
+230158634
+CPSC 231 - Assignment 4
+15-11-2025
+*/
+
+#include <stdio.h>
+
+#define NUMBER_OF_CUSTOMERS 5      
+#define NUMBER_OF_RESOURCES 4      
+
+int available[NUMBER_OF_RESOURCES];
+int maximum[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
+int main() {
+    printf("Banker's Algorithm\n");
+    return 0;
+}
